cd28627349efcdc0be0635dd5dbc01387b473158
dekapaan <terblancheronald@gmail.com>
2021-07-29 12:14:32 +0200

    updated

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@
 function openModal() {
   document.getElementById("modal").classList.toggle("modal-active");
 }
+
+// function takes in id and displays the testimonial
+function showTestimonial(id) {
+  //this is to obtain all the testimonials
+  document.querySelectorAll(".test-slider").forEach((test) => {
+    //for each testimonial remove active class
+    test.classList.remove("test-active");
+  });
+  //add active class to the testimonial ID received
+  document.querySelector(`${id}`).classList.add("test-active");
+}
